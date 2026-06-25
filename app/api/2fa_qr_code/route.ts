6ba93b1ab176f98900aa358a2d3b40eb2763c66b
Aymen-Braikia <aymenbraikia1@gmail.com>
2026-06-25 20:29:33 +0100

    fixed otplib

--- app/api/2fa_qr_code/route.ts
+++ app/api/2fa_qr_code/route.ts
@@ -1,38 +1,37 @@
 "use server";
-import { authenticator } from "otplib";
+import { generateSecret, generateURI } from "otplib";
 import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 import { verifyJwt } from "@/lib/jwt";
 import { userCollection } from "@/app/db/collections";
 
 async function generate_qrcode(email: string): Promise<string | null> {
+    const user = await userCollection.findOne({ email: email });
+    if (!user) return null;
 
-	const user = await userCollection.findOne({ email: email });
-	if (!user) return null;
+    const secret = generateSecret();
 
-	const secret = authenticator.generateSecret();
+    await userCollection.updateOne({ email: email }, { $set: { "twoFactorAuth.secret": secret } });
 
-	await userCollection.updateOne({ email: email }, { $set: { "twoFactorAuth.secret": secret } });
-
-	const uri = authenticator.keyuri(user.email, "hosty", secret);
-	return uri;
+    const uri = generateURI({ issuer: user.email, label: "hosty", secret });
+    return uri;
 }
 
 export async function GET() {
-	const cookieStore = await cookies();
+    const cookieStore = await cookies();
 
-	const token = cookieStore.get("accessToken")?.value;
+    const token = cookieStore.get("accessToken")?.value;
 
-	let payload = null;
+    let payload = null;
 
-	if (!token) return NextResponse.json({ redirect: "/login" }, { status: 401 });
-	else {
-		payload = verifyJwt(token);
+    if (!token) return NextResponse.json({ redirect: "/login" }, { status: 401 });
+    else {
+        payload = verifyJwt(token);
 
-		if (!payload) return NextResponse.json({ redirect: "/login" }, { status: 401 });
-	}
+        if (!payload) return NextResponse.json({ redirect: "/login" }, { status: 401 });
+    }
 
-	const qr_code = await generate_qrcode(payload.email);
-	if (!qr_code) return new Response("Error generating QR code", { status: 500 });
-	return new Response(qr_code, { status: 200 });
+    const qr_code = await generate_qrcode(payload.email);
+    if (!qr_code) return new Response("Error generating QR code", { status: 500 });
+    return new Response(qr_code, { status: 200 });
 }
