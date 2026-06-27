66b0f688351b6ab10c98b96a4680c93ddb0ed6fd
Aymen-Braikia <aymenbraikia1@gmail.com>
2026-06-27 17:36:38 +0100

    optmized a bit

--- app/[locale]/body.tsx
+++ app/[locale]/body.tsx
@@ -1,30 +1,34 @@
-import LoadingHero from "./components/loading/hero";
-import LoadingTechStackScroll from "./components/loading/slider";
+// import LoadingHero from "./components/loading/hero";
+// import LoadingTechStackScroll from "./components/loading/slider";
 import dynamic from "next/dynamic";
 
-const Hero = dynamic(() => import("./components/hero"), { ssr: true, loading: () => <LoadingHero /> });
-const TechStackScroll = dynamic(() => import("./components/slider"), { ssr: true, loading: () => <LoadingTechStackScroll /> });
+// const Hero = dynamic(() => import("./components/hero"), { ssr: true, loading: () => <LoadingHero /> });
+// const TechStackScroll = dynamic(() => import("./components/slider"), { ssr: true, loading: () => <LoadingTechStackScroll /> });
+
+import Hero from "./components/hero";
+import TechStackScroll from "./components/slider";
 
 const Cta = dynamic(() => import("./components/cta"), { ssr: true, loading: () => <></> });
 const Faq = dynamic(() => import("./components/faq"), { ssr: true, loading: () => <></> });
-const Network = dynamic(() => import("./components/network"), { ssr: true, loading: () => <></> });
 const OneClickDeploy = dynamic(() => import("./components/oneClickDeploy"), { ssr: true, loading: () => <></> });
 const Perks = dynamic(() => import("./components/perks"), { ssr: true, loading: () => <></> });
 const Pricing = dynamic(() => import("./components/pricing"), { ssr: true, loading: () => <></> });
 const Trust = dynamic(() => import("./components/trust"), { ssr: true, loading: () => <></> });
 
+const Network = dynamic(() => import("./components/network"), { ssr: false, loading: () => <></> });
+
 export default function Body() {
-	return (
-		<div className="translate-y-16 flex flex-col justify-center items-start">
-			<Hero />
-			<TechStackScroll />
-			<Perks />
-			<OneClickDeploy />
-			<Network />
-			<Pricing />
-			<Trust />
-			<Faq />
-			<Cta />
-		</div>
-	);
+    return (
+        <div className="translate-y-16 flex flex-col justify-center items-start">
+            <Hero />
+            <TechStackScroll />
+            <Perks />
+            <OneClickDeploy />
+            <Network />
+            <Pricing />
+            <Trust />
+            <Faq />
+            <Cta />
+        </div>
+    );
 }
